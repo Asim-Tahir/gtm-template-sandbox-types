1f0d7e947ea0be4bb9df89f332e7f29b85627878
Asım Tahir <AsimTahir@protonmail.com>
2026-03-12 06:22:36 +0300

    fix: use imports instead instead of export all in web sandbox types

--- types/web.d.ts
+++ types/web.d.ts
@@ -1,7 +1,3 @@
-export * from "./common/data";
-export * from "./common/require";
-export * from "./common/module";
-
-// import "./common/data";
-// import "./common/require";
-// import "./common/module";
+import "./common/data";
+import "./common/module";
+import "./common/require";
